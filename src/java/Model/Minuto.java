90c2d57be1fb36a2f919b9c84f00249b86198724
Aurum CEO <Aurum CEO@10.0.1.142>
2014-06-02 21:58:47 -0500

    respaldo 2/junio/12

---
+++ src/java/Model/Minuto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Aurum CEO
+ */
+public class Minuto {
+
+    String idMinuto, idHorario, idioma, minuto, descripcion;
+
+    public Minuto(String idM,
+            String idH,
+            String idi,
+            String min,
+            String des) {
+        idMinuto = idH;
+        idHorario = idH;
+        idioma = idi;
+        minuto = min;
+        descripcion = des;
+
+    }
+}
